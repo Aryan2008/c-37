db3683417ad5894a17481971cff9f86759347dd7
Aryan2008 <64368042+Aryan2008@users.noreply.github.com>
2020-07-13 16:37:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, database;
+var backGroundImg;
+var gameState=0;
+var playerCount;
+var form,player,game;
+var allPlayers; 
+var cars , car1,car2,car3,car4;
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  Canvas=createCanvas(displayWidth -20,displayHeight -30);
+
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+ if(playerCount===4){
+   game.update(1);
+ } 
+ if(gameState===1){
+   clear();
+   game.play();
+ }
+}
+
